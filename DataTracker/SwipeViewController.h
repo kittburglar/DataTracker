4f1511a4f3d3a6ef98854a2ae18c62b6c70c1d48
Kittiphong Xayasane <kxayasan@gmail.com>
2015-06-10 03:09:47 -0400

    Added swipe view to decrease number number of menu items.

---
+++ DataTracker/SwipeViewController.h
@@ -0,0 +1,17 @@
+//
+//  SwipeViewController.h
+//  DataTracker
+//
+//  Created by kittiphong xayasane on 2015-06-10.
+//  Copyright (c) 2015 Kittiphong Xayasane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SwipeView.h"
+
+@interface SwipeViewController : UIViewController <SwipeViewDataSource, SwipeViewDelegate>
+
+@property (nonatomic, weak) IBOutlet SwipeView *swipeView;
+@property (nonatomic, strong) NSMutableArray *items;
+
+@end
---
+++ DataTracker/SwipeViewController.m
@@ -0,0 +1,133 @@
+//
+//  SwipeViewController.m
+//  DataTracker
+//
+//  Created by kittiphong xayasane on 2015-06-10.
+//  Copyright (c) 2015 Kittiphong Xayasane. All rights reserved.
+//
+
+#import "SwipeViewController.h"
+
+@interface SwipeViewController () <SwipeViewDataSource, SwipeViewDelegate>
+
+@end
+
+@implementation SwipeViewController
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Navigation
+/*
+ //In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    [segue self];
+}
+*/
+
+
+
+- (void)dealloc
+{
+    //it's a good idea to set these to nil here to avoid
+    //sending messages to a deallocated viewcontroller
+    //this is true even if your project is using ARC, unless
+    //you are targeting iOS 5 as a minimum deployment target
+    _swipeView.delegate = nil;
+    _swipeView.dataSource = nil;
+}
+
+#pragma mark -
+#pragma mark View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"SwipeView view did load");
+    self.items = [NSMutableArray array];
+    for (int i = 0; i < 100; i++)
+    {
+        [_items addObject:@(i)];
+    }
+    _swipeView.delegate = self;
+    _swipeView.dataSource = self;
+    [_swipeView reloadData];
+    _swipeView.alignment = SwipeViewAlignmentCenter;
+    //configure swipeView
+    _swipeView.pagingEnabled = YES;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+#pragma mark -
+#pragma mark iCarousel methods
+
+- (NSInteger)numberOfItemsInSwipeView:(SwipeView *)swipeView
+{
+    //return the total number of items in the carousel
+    return 100;
+}
+
+- (UIView *)swipeView:(SwipeView *)swipeView viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view
+{
+    NSLog(@"viewForItemAtIndex");
+    UILabel *label = nil;
+    
+    //create new view if no view is available for recycling
+    if (view == nil)
+    {
+        //don't do anything specific to the index within
+        //this `if (view == nil) {...}` statement because the view will be
+        //recycled and used with other index values later
+        view = [[UIView alloc] initWithFrame:self.swipeView.bounds];
+        view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        
+        label = [[UILabel alloc] initWithFrame:view.bounds];
+        label.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        label.backgroundColor = [UIColor clearColor];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.font = [label.font fontWithSize:50];
+        label.tag = 1;
+        [view addSubview:label];
+    }
+    else
+    {
+        //get a reference to the label in the recycled view
+        label = (UILabel *)[view viewWithTag:1];
+    }
+    
+    //set background color
+    CGFloat red = arc4random() / (CGFloat)INT_MAX;
+    CGFloat green = arc4random() / (CGFloat)INT_MAX;
+    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+    view.backgroundColor = [UIColor colorWithRed:red
+                                           green:green
+                                            blue:blue
+                                           alpha:1.0];
+    
+    //set item label
+    //remember to always set any properties of your carousel item
+    //views outside of the `if (view == nil) {...}` check otherwise
+    //you'll get weird issues with carousel item content appearing
+    //in the wrong place in the carousel
+    label.text = [_items[index] stringValue];
+    
+    return view;
+}
+
+- (CGSize)swipeViewItemSize:(SwipeView *)swipeView
+{
+    return self.swipeView.bounds.size;
+}
+
+@end
